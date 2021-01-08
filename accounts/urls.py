4065a7fb267d0fad4ca258ce67148bb6af5fee3a
Vijayavengadam <vijayavengadam0403@gmail.com>
2021-01-08 10:35:30 +0530

    initial commit

---
+++ accounts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from.import views
+from django.contrib.auth.views import LoginView,LogoutView
+
+urlpatterns = [
+
+       path('',views.indexview,name="home"),
+       path('dashboard/',views.dashboardView,name="dashboard"),
+       path('login/',LoginView.as_view(),name="login_url"),
+       path('register/',views.registerView,name="register_url"),
+       path('logout/',LogoutView.as_view(next_page='dashboard'),name="logout"),
+   
+]
---
+++ accounts/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+def indexview(request):
+	return render(request,'index.html')
+	
+@login_required
+
+def dashboardView(request):
+	return render(request,'dashboard.html')
+
+def registerView(request):
+	if request.method == "POST":
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('login_url')
+	else:
+		form = UserCreationForm()
+
+	return render(request,'registration/register.html',{'form':form})
